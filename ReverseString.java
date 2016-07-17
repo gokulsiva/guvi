8c45b105795b4ae36a1a0f7601826b0aab3f079b
gokulsiva <gsgokul305@gmail.com>
2016-07-17 23:35:58 +0530

    Reverse a string

---
+++ ReverseString.java
@@ -0,0 +1,12 @@
+
+public class ReverseNumber {
+
+	public static void main(String[] args) {
+		
+
+		StringBuffer buffer = new StringBuffer(args[0]);
+		System.out.println("Reversed number : "+buffer.reverse());
+
+	}
+
+}
